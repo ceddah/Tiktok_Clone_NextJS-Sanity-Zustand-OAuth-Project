50f4e65cc8682c5766301c65a14824aca8f1c3d2
ceddah <cedanik1@gmail.com>
2022-07-12 20:16:16 +0200

    Upload video page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import GoogleLogin from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
 import Discover from "./Discover";
@@ -11,7 +10,6 @@ import Footer from "./Footer";
 
 const Sidebar: NextPage = () => {
   const [showSidebar, setShowSidebar] = useState(true);
-  const userProfile = false;
   const normalLink =
     "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded";
   return (
@@ -34,28 +32,6 @@ const Sidebar: NextPage = () => {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className="px-2 py-4 hidden xl:block">
-              <p className="text-gray-400">Log in to like and comment on videos</p>
-              <div className="pr-4">
-                <GoogleLogin
-                  clientId=""
-                  render={(renderProps) => (
-                    <button
-                      className="bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded outline-none w-full mt-3 hover:text-white hover:bg-[#F51997] cursor-pointer transition-all"
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                    >
-                      Log In
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy="single-host-origin"
-                />
-              </div>
-            </div>
-          )}
           <Discover />
           <SuggestedAccounts />
           <Footer />
